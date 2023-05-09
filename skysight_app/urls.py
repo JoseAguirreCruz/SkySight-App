bf890def03a87d31b063628620e5a698ec1f5b43
Jose Aguirre <aguirrejose615@gmail.com>
2023-05-09 14:35:44 -0400

    implementing auth

--- skysight_app/urls.py
+++ skysight_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('flight/index/', views.flight_view, name='index'),
   path('weather/index/', views.weather_view, name='weather'),
+  path('accounts/', include('django.contrib.auth.urls')),
 ]
