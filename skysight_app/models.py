d76e0490c159e26fd9cd5f29a4f34c34fcb1bc8a
Jose Aguirre <aguirrejose615@gmail.com>
2023-05-11 15:26:52 -0400

    implemented local time in nav

--- skysight_app/models.py
+++ skysight_app/models.py
@@ -15,3 +15,4 @@ class WeatherData(models.Model):
     condition = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
